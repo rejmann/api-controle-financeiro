0729f739303343906a6d8c7bf3669365087f5417
Rejman Nascimento <rejmans00@gmail.com>
2022-01-25 11:42:05 -0300

    Criado migrations de motimentação/tipos. #6

---
+++ database/migrations/2022_01_25_124405_create_moviment_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMovimentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('moviment', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('description');
+            $table->double('value');
+            $table->date('date');
+
+            $table->unsignedBigInteger('types_id');
+            $table->foreign('types_id')
+                ->references('id')
+                ->on('types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('moviment');
+    }
+}
