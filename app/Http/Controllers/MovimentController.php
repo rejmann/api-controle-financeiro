a3b764e176ce68bebd0d39cd4b1cfd7885defce1
Rejman Nascimento <rejmans00@gmail.com>
2022-01-26 11:38:38 -0300

    Implementado método show para busca por id. #16

--- app/Http/Controllers/MovimentController.php
+++ app/Http/Controllers/MovimentController.php
@@ -11,11 +11,10 @@ class MovimentController extends Controller
     public function index(Request $request)
     {
         /**
-         * Recupera a URI da rota e retira o último caractere
+         * Recupera da URI os primeiros 7 caracteres que é referente ao tipo de movimentação
          * Busca o primeiro tipo
          */
-        $type = Type::where('name', substr($request->path(),0, -1))
-            ->first();
+        $type = Type::where('name', substr($request->path(),0, 7))->first();
 
         /**
          * Returna todos as movimentações em formato json do tipo atribuído na URI da rota
@@ -28,10 +27,10 @@ class MovimentController extends Controller
     public function store(Request $request)
     {
         /**
-         * Recupera a URI da rota e retira o último caractere
+         * Recupera da URI os primeiros 7 caracteres que é referente ao tipo de movimentação
          * Busca o primeiro tipo
          */
-        $type = Type::where('name', substr($request->path(),0, -1))->first();
+        $type = Type::where('name', substr($request->path(),0, 7))->first();
 
         /**
          * Retorna o cadastro em json da movimentação criada
@@ -43,6 +42,32 @@ class MovimentController extends Controller
                 'date' => $request->date,
                 'types_id' => $type->id,
             ]), 200);
+    }
+
+    public function show(Request $request, int $id)
+    {
+        /**
+         * Recupera da URI os primeiros 7 caracteres que é referente ao tipo de movimentação
+         * Busca o primeiro tipo
+         */
+        $type = Type::where('name', substr($request->path(),0, 7))->first();
+
+        /**
+         * Busca a primeira movimentação por id/types_id
+         */
+        $data = Moviment::all()->where('id', $id)->where('types_id', $type->id)->first();
+
+        /**
+         * Valida se a movimentação existe
+         */
+        if(!$data){
+            return response()->json(['no_exist' => 'Não foi encontrado movimentação para este id.']);
+        }
+
+        /**
+         * Retorna a busca em json da movimentação encontrada
+         */
+        return response()->json($data, 200);
     }
 
 }
