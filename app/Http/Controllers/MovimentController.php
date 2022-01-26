1f45beffc87e86a2d2a17512e8536d5153e91393
Rejman Nascimento <rejmans00@gmail.com>
2022-01-25 21:18:38 -0300

    Modificado método index para retornar todas movimentações a partir da URI/tipo informada. #13

--- app/Http/Controllers/MovimentController.php
+++ app/Http/Controllers/MovimentController.php
@@ -6,23 +6,43 @@ use App\Models\Moviment;
 use App\Models\Type;
 use Illuminate\Http\Request;
 
-class MovimentController
+class MovimentController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return Moviment::all();
+        /**
+         * Recupera a URI da rota e retira o último caractere
+         * Busca o primeiro tipo
+         */
+        $type = Type::where('name', substr($request->path(),0, -1))
+            ->first();
+
+        /**
+         * Returna todos as movimentações em formato json do tipo atribuído na URI da rota
+         */
+        return response()
+            ->json(Moviment::all()
+                ->where('types_id', $type->id), 200);
     }
 
     public function store(Request $request)
     {
-        $type = Type::where('name', $request->type)->first();
+        /**
+         * Recupera a URI da rota e retira o último caractere
+         * Busca o primeiro tipo
+         */
+        $type = Type::where('name', substr($request->path(),0, -1))->first();
 
+        /**
+         * Retorna o cadastro em json da movimentação criada
+         */
         return response()
             ->json(Moviment::created([
                 'description' => $request->description,
                 'value' => $request->value,
                 'date' => $request->date,
                 'types_id' => $type->id,
-            ]), 201);
+            ]), 200);
     }
+
 }
