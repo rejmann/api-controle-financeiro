7035aa07e8e5c70f1afb65bf74eb04802e237dfb
Rejman Nascimento <rejmans00@gmail.com>
2022-01-26 11:03:40 -0300

    Criado seed de movimentação. #15

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(TypeSeeder::class);
+        $this->call([
+            TypeSeeder::class,
+            MovimentSeeder::class,
+        ]);
     }
 }
---
+++ database/seeders/MovimentSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MovimentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $types = Type::all();
+
+        for($i = 1; $i <= 10; $i++)
+        {
+            DB::table('moviments')->insert([
+                'description' => "Movimentação {$i}",
+                'value' => rand(1,999),
+                'date' => date('Y-m-d'),
+                'types_id' => $types->random()->id,
+            ]);
+        }
+
+    }
+}
