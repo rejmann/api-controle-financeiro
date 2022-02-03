4f28d475c61fa0aec7d58fcab34ab64dcdb1d227
Rejman Nascimento <rejmans00@gmail.com>
2022-02-03 08:53:20 -0300

    Middleware para consumo da API se autenticado. #43

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\User;
+use Firebase\JWT\JWT;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -31,9 +32,15 @@ class AuthServiceProvider extends ServiceProvider
         // the User instance via an API token or any other method necessary.
 
         $this->app['auth']->viaRequest('api', function ($request) {
-            if ($request->input('api_token')) {
-                return User::where('api_token', $request->input('api_token'))->first();
+            if (!$request->hasHeader('Authorization')) {
+                return null;
             }
+            $authorizationHeader = $request->header('Authorization');
+            $token = str_replace('Bearer ', '', $authorizationHeader);
+
+            $authenticationData = JWT::decode($token,'', ['HS256']);
+
+            return User::where('email', $authenticationData['email'])->first();
         });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,39 +19,39 @@ $router->get('/', function () use ($router) {
 });
 
 /**
- * Grupo de rotas para receitas
+ * Middleware
  */
-$router->group(['prefix' => 'receitas'], function () use ($router) {
+$router->group(['middleware' => 'auth'], function () use ($router) {
+    /**
+     * Grupo de rotas para receitas
+     */
+    $router->group(['prefix' => 'receitas', 'middleware' => 'auth'], function () use ($router) {
 
-    $router->get('', 'MovimentController@index');
-    $router->get('{id}', 'MovimentController@show');
-    $router->get('{year}/{month}', 'MovimentController@showByMonth');
-    $router->post('', 'MovimentController@store');
-    $router->put('{id}', 'MovimentController@update');
-    $router->delete('{id}', 'MovimentController@destroy');
+        $router->get('', 'MovimentController@index');
+        $router->get('{id}', 'MovimentController@show');
+        $router->get('{year}/{month}', 'MovimentController@showByMonth');
+        $router->post('', 'MovimentController@store');
+        $router->put('{id}', 'MovimentController@update');
+        $router->delete('{id}', 'MovimentController@destroy');
 
+    });
+
+    /**
+     * Grupo de rotas para despesas
+     */
+    $router->group(['prefix' => 'despesas', 'middleware' => 'auth'], function () use ($router) {
+
+        $router->get('', 'MovimentController@index');
+        $router->get('{id}', 'MovimentController@show');
+        $router->get('{year}/{month}', 'MovimentController@showByMonth');
+        $router->post('', 'MovimentController@store');
+        $router->put('{id}', 'MovimentController@update');
+        $router->delete('{id}', 'MovimentController@destroy');
+
+    });
+
+    /**
+     * Rota para resumo
+     */
+    $router->get('resumo/{year}/{month}', 'ResumeController@showByMonth');
 });
-
-/**
- * Grupo de rotas para despesas
- */
-$router->group(['prefix' => 'despesas'], function () use ($router) {
-
-    $router->get('', 'MovimentController@index');
-    $router->get('{id}', 'MovimentController@show');
-    $router->get('{year}/{month}', 'MovimentController@showByMonth');
-    $router->post('', 'MovimentController@store');
-    $router->put('{id}', 'MovimentController@update');
-    $router->delete('{id}', 'MovimentController@destroy');
-
-});
-
-/**
- * Rota para resumo
- */
-$router->get('resumo/{year}/{month}', 'ResumeController@showByMonth');
-
-
-
-
-
