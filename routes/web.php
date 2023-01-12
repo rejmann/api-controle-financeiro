67e6e025d2491418030bc6504de840cc1ed76e13
Rejman <rejmans00@gmail.com>
2023-01-11 22:48:17 -0300

    (improv): Definindo prefixo api/

--- routes/web.php
+++ routes/web.php
@@ -9,29 +9,31 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->post('login', 'LoginController@index');
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->post('login', 'LoginController@index');
 
-$router->post('cadastrar', 'UserController@store');
+    $router->post('cadastrar', 'UserController@store');
 
-$router->group(['middleware' => 'auth'], function () use ($router) {
+    $router->group(['middleware' => 'auth'], function () use ($router) {
 
-    $router->group(['prefix' => 'receitas'], function () use ($router) {
-        $router->get('', 'MovimentController@indexRevenueAction');
-        $router->get('{id}', 'MovimentController@showRevenueAction');
-        $router->post('', 'MovimentController@storeRevenueAction');
-        $router->get('{year}/{month}', 'MovimentController@showRevenueByMonth');
-        $router->put('{id}', 'MovimentController@updateRevenueAction');
-        $router->delete('{id}', 'MovimentController@destroy');
+        $router->group(['prefix' => 'receitas'], function () use ($router) {
+            $router->get('', 'MovimentController@indexRevenueAction');
+            $router->get('{id}', 'MovimentController@showRevenueAction');
+            $router->post('', 'MovimentController@storeRevenueAction');
+            $router->get('{year}/{month}', 'MovimentController@showRevenueByMonth');
+            $router->put('{id}', 'MovimentController@updateRevenueAction');
+            $router->delete('{id}', 'MovimentController@destroy');
+        });
+
+        $router->group(['prefix' => 'despesas'], function () use ($router) {
+            $router->get('', 'MovimentController@indexExpenseAction');
+            $router->get('{id}', 'MovimentController@showExpenseAction');
+            $router->post('', 'MovimentController@storeExpenseAction');
+            $router->get('{year}/{month}', 'MovimentController@showExpenseByMonth');
+            $router->put('{id}', 'MovimentController@updateExpenseAction');
+            $router->delete('{id}', 'MovimentController@destroy');
+        });
+
+        $router->get('resumo/{year}/{month}', 'ResumeController@index');
     });
-
-    $router->group(['prefix' => 'despesas'], function () use ($router) {
-        $router->get('', 'MovimentController@indexExpenseAction');
-        $router->get('{id}', 'MovimentController@showExpenseAction');
-        $router->post('', 'MovimentController@storeExpenseAction');
-        $router->get('{year}/{month}', 'MovimentController@showExpenseByMonth');
-        $router->put('{id}', 'MovimentController@updateExpenseAction');
-        $router->delete('{id}', 'MovimentController@destroy');
-    });
-
-    $router->get('resumo/{year}/{month}', 'ResumeController@index');
 });
